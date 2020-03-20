15d92c70e3d900d7fbe2eb8f4758c880ebeb27dd
Tony Papasifakis <tony.papasifakis@hotmail.co.uk>
2020-03-20 16:33:36 +0000

    commit

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
